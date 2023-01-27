927ce23ce3f4d82213dffd96ca297ca639d97487
Alexandria <alxpettit@gmail.com>
2023-01-27 16:26:39 -0700

    easier to read

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,18 @@ use rustfft::FftPlanner;
 fn main() {
     // create a test input vector
     let input_buf = vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0];
-    println!("Input buffer: {:?}", input_buf);
+    println!("Input buffer: {:?}\n", input_buf);
     // create a planner for the FFT
     let mut planner = FftPlanner::new();
     let fft = planner.plan_fft_forward(input_buf.len());
 
     // convert the input to complex numbers
     let mut buf: Vec<Complex<f32>> = input_buf.iter().map(|x| Complex::new(*x, 0.0)).collect();
-    println!("Input buffer (complex): {:?}", buf);
+    println!("Input buffer (complex): {:?}\n", buf);
 
     // perform the FFT
     fft.process(&mut buf);
-    println!("Post-FFT: {:?}", buf);
+    println!("Post-FFT: {:?}\n", buf);
 
     // create a planner for the IDFT
     let mut planner = FftPlanner::new();
@@ -23,7 +23,7 @@ fn main() {
 
     // perform the IDFT
     ifft.process(&mut buf);
-    println!("Post-IDFT: {:?}", buf);
+    println!("Post-IDFT: {:?}\n", buf);
 
     let buf_len = buf.len();
     // Normalize the output
@@ -33,7 +33,7 @@ fn main() {
 
     // extract the real parts of the output
     let output_real: Vec<f32> = buf.iter().map(|x| x.re).collect();
-    println!("Output buffer (real): {:?}", output_real);
+    println!("Output buffer (real): {:?}\n", output_real);
 
     // check that the output is equal to the original input
     assert_eq!(input_buf, output_real);
